0b28be0ff3f8a1f21986fe0adf271601e4dea945
nicogc001 <you@example.com>
2025-03-10 22:49:13 +0100

    controller

--- pasteleria-backend/routes/authRoutes.js
+++ pasteleria-backend/routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController'); // Verifica que existe
+const authController = require('../controllers/authController'); // Verifica que esta ruta es correcta
+
+// Asegúrate de que authController tiene las funciones definidas
+if (!authController.register || !authController.login) {
+  console.error("❌ Error: Las funciones register o login no están definidas en authController.");
+}
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
