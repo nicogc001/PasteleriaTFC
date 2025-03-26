60099ac53c6af41d5c08ddcd10f94da2a7d7a827
nicogc001 <you@example.com>
2025-03-26 18:35:58 +0100

    cambios pedido_admin

--- backend/routes/productosRoutes.js
+++ backend/routes/productosRoutes.js
@@ -86,4 +86,21 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+// Cambiar el estado de un pedido
+router.put('/:id/estado', authMiddleware, async (req, res) => {
+    try {
+      const pedido = await Pedidos.findByPk(req.params.id);
+      if (!pedido) return res.status(404).json({ error: 'Pedido no encontrado' });
+  
+      pedido.estado = req.body.estado;
+      await pedido.save();
+  
+      res.json({ message: 'Estado actualizado correctamente', pedido });
+    } catch (err) {
+      console.error("❌ Error actualizando estado:", err);
+      res.status(500).json({ error: 'Error en el servidor' });
+    }
+  });
+  
+
 module.exports = router;
