d9101af65c469a73cd7331570d4816ca541aae5e
nicogc001 <you@example.com>
2025-04-18 20:24:31 +0200

    menuEmpleado

--- scripts/cart.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pedido = JSON.parse(localStorage.getItem('pedido'));
-    const pedidoInfo = document.getElementById('pedido-info');
-    
-    if (pedido && pedidoInfo) {
-        pedidoInfo.innerHTML = 
-            `Pedido: <strong>${pedido.tipo}</strong> para el <strong>${pedido.fecha}</strong>`;
-    }
-});
