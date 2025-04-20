89230174795c3e0cb464ec5c2a70f32483076a09
nicogc001 <you@example.com>
2025-04-20 22:18:26 +0200

    2

--- backend/routes/empleoRoutes.js
+++ backend/routes/empleoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const router = express.Router();
-const { crearSolicitud, obtenerSolicitudes } = require('../models/solicitudEmpleoModel');
+const { crearSolicitud, obtenerSolicitudes } = require('../models/SolicitudEmpleo');
 
 // Configuración multer para solo aceptar PDF
 const storage = multer.diskStorage({
